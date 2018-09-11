ca593a8f76cad6f50a3b82dd88292593047c7574
logonrmlocal <logonrmlocal@L2201MICRO19>
2018-09-10 21:23:26 -0300

    vs 1

---
+++ Repository/UserProfileMongo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SimpleBot.Repository
+{
+    [BsonIgnoreExtraElements]
+    public class UserProfileMongo
+    {
+        [BsonElement]
+        public string _id { get; set; }
+        [BsonElement]
+        public int Visitas { get; set; }
+    }
+}
